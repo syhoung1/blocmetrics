a0e669029702054a498f6549fb7716329f5259f1
Steven <syhoung1@gmail.com>
2017-05-21 08:29:49 +0000

    styling fixes

--- app/controllers/registered_apps_controller.rb
+++ app/controllers/registered_apps_controller.rb
@@ -1,13 +1,12 @@
 class RegisteredAppsController < ApplicationController
-  # before_action :check_url, only: [:create, :update]
-  
   def index
     @registered_apps = current_user.registered_apps
   end
+
   def new
     @registered_app = RegisteredApp.new
   end
-  
+
   def create
     @registered_app = current_user.registered_apps.new(app_params)
     
@@ -18,16 +17,16 @@ class RegisteredAppsController < ApplicationController
       render :new
     end
   end
-  
+
   def show
     @registered_app = current_user.registered_apps.find(params[:id])
     @events = @registered_app.events.group_by(&:name)
   end
-  
+
   def edit
     @registered_app = RegisteredApp.find(params[:id])
   end
-  
+
   def update
     @registered_app = RegisteredApp.find(params[:id])
     @registered_app.assign_attributes(app_params)
@@ -39,16 +38,8 @@ class RegisteredAppsController < ApplicationController
       render :edit
     end
   end
-  
+
   def app_params
     params.require(:registered_app).permit(:name, :url)
-  end
-  
-  def check_url
-    urlexp = /^((http[s]?|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/
-    if !params[:registered_app][:url].match(urlexp)
-      flash[:alert] = "That is not a valid URL"
-      redirect_to :back
-    end
   end
 end
--- app/models/registered_app.rb
+++ app/models/registered_app.rb
@@ -1,4 +1,5 @@
 class RegisteredApp < ActiveRecord::Base
   belongs_to :user
   has_many :events
+  validates :url, url: true, presence: true
 end
